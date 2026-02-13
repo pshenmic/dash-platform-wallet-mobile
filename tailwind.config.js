9311c45e4359570cfaa43e80669681ee9b456104
Alexey <andreewsky.alexey@gmail.com>
2026-02-13 18:40:29 +0300

    Upload files for build

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        'dash-brand': '#008DE4',
+        'dash-mint': '#00D4AA',
+      },
+    },
+  },
+  plugins: [],
+}
